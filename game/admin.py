4948699cc12980d01c6e2a8aa697212f4ed3375e
hyunwoo <bhw9102@gmail.com>
2019-05-01 15:24:02 +0900

    django admin에서 보이도록 하다.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from game.models import CardClass
 
-# Register your models here.
+
+class CardClassAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'color_front', 'color_back', 'count',)
+    list_display_links = ('id', )
+
+
+admin.site.register(CardClass, CardClassAdmin)
+
--- session/admin.py
+++ session/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from session.models import Player, Session, PlayerSession, Card
 
-# Register your models here.
+
+class PlayerAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', )
+    list_display_links = ('id', 'name', )
+
+
+class SessionAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', 'state', )
+    list_display_links = ('id', 'name', 'state', )
+
+
+class PlayerSessionAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'player', 'session', 'score', )
+    list_display_links = ('id', 'score', )
+
+
+class CardAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'session', 'location', 'player_session', )
+    list_display_links = ('id', )
+
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(PlayerSession, PlayerSessionAdmin)
+admin.site.register(Card, CardAdmin)
