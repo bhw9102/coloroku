bb2c84baaea17f185e0336b7b9f076d31f8795ce
hyunwoo <bhw9102@gmail.com>
2019-05-01 16:11:27 +0900

    웹페이지가 보이도록 하다.

--- coloroku/urls.py
+++ coloroku/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
+from django.views.generic import RedirectView
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('session/', include('session.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
